bc99e8be38bef33ba6a7a95f3ab319290e2a84be
Marty Rath <martydavidger@gmail.com>
2024-02-23 12:06:30 +0000

    Instance names assigned by time (HH:MM)

--- create_instance.py
+++ create_instance.py
@@ -3,18 +3,23 @@
 # Description: Python 3 program to create an ec2 instance.
 
 import boto3
+import time
 from user_data_script import generate_user_data_script
 
 def create_instance():
+  ec2 = boto3.resource('ec2')
+  
   # Ensure this is up to date
   ami_id = 'ami-0440d3b780d96b29d'
   
-  try:
-    ec2 = boto3.resource('ec2')
-  
-    # Assign instance name
-    instance_name = "whatever"
+  # Using time to name instance
+  local_time = time.localtime()
+  format_time = time.strftime("%H%M", local_time)
 
+  # Assign instance name
+  instance_name = "Instance: " + format_time
+
+  try:
     instance = ec2.create_instances(
     ImageId=ami_id,
     MinCount=1,
--- user_data_script.py
+++ user_data_script.py
@@ -3,27 +3,27 @@
 # Description: Generates a user data script for an ec2 instance.
 
 def generate_user_data_script(instance_name):
-    script = f"""#!/bin/bash 
-    # apply any required patches to the operating system
-    yum update -y
-    yum install httpd -y
-    systemctl enable httpd
-    systemctl start httpd
-    
-    # Create IMDSv2 session token
-    TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-    
-    # Write metadata to index.html
-    # Creates index.html and adds html and body
-    echo "<html><body>" > /var/www/html/index.html
-    echo "Your instance name is: {instance_name} <br>" >> /var/www/html/index.html
-    echo "<hr>This instance is running in availability zone: " >> /var/www/html/index.html
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
-    echo "<hr>The instance ID is: " >> /var/www/html/index.html
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
-    echo "<hr>The instance type is: " >> /var/www/html/index.html
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type >> /var/www/html/index.html
-    echo "Here is your image stored on S3: <br> <img src="https://lab2bucket28jan.s3.amazonaws.com/sunflower.avif" width="500" height="500">" >> /var/www/html/index.html
-    echo "</body></html>" >> /var/www/html/index.html
-    """
-    return script
+  script = f"""#!/bin/bash 
+  # apply any required patches to the operating system
+  yum update -y
+  yum install httpd -y
+  systemctl enable httpd
+  systemctl start httpd
+  
+  # Create IMDSv2 session token
+  TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+
+  # Write metadata to index.html
+  # Creates index.html and adds html and body
+  echo "<html><body>" > /var/www/html/index.html
+  echo "Your instance name is: {instance_name} <br>" >> /var/www/html/index.html
+  echo "<hr>This instance is running in availability zone: " >> /var/www/html/index.html
+  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/index.html
+  echo "<hr>The instance ID is: " >> /var/www/html/index.html
+  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/index.html
+  echo "<hr>The instance type is: " >> /var/www/html/index.html
+  curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-type >> /var/www/html/index.html
+  echo "<hr>Here is your image stored on S3: <br> <img src="https://lab2bucket28jan.s3.amazonaws.com/sunflower.avif" width="500" height="500">" >> /var/www/html/index.html
+  echo "</body></html>" >> /var/www/html/index.html
+  """
+  return script
